ab309f91f79241b048cffc02eafcddf8d71a7ba0
Yash Kaskar <yash.kaskar99@gmail.com>
2022-10-14 14:56:48 +0530

    first commit

---
+++ Readme.md
@@ -0,0 +1,14 @@
+Yash Sanjay Kaskar
+Pursuing as an  -  Full Stack JavaScript Web Developer
+
+
+++++++++++++++++++👨‍💻  Document Object Model  👨‍💻++++++++++++++++++
+
+In this project/assignment. I have played with website([Code Wars](https://www.codewars.com/))
+
+In this website i have changed Font Family, Color of Text.
+
+For this used the Dom Elements which made me more easy to work on it. 
+I used <Query Selctor> <Style> <Color> <Font Family>
+
+
---
+++ app.js
@@ -0,0 +1,20 @@
+// Webiste Name: [Code Wars](https://www.codewars.com/)
+
+// ### Topics
+
+//        Change Font Family, Color of Text.
+
+
+
+// Write your code here:
+
+document.querySelector('.display-heading-1');
+<h1 class="display-heading-1">​Achieve Mastery Through Challenge​</h1>​
+
+document.querySelector('.display-heading-1').style .color = 'red';
+'red'
+
+document.querySelector('.display-heading-1').style .fontFamily = 'Fuzzy Bubbles';
+'Fuzzy Bubbles'
+
+
